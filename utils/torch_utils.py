db936acb75bc571a93b756dd332b711f20cc4746
NanoCode012 <kevinvong@rocketmail.com>
2020-07-07 20:36:23 +0700

    Update following main repo. Removes pickle.
    Cleaner code, removes try except, and removes use of pickle for forward compatibility.

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -210,22 +210,14 @@ class ModelEMA:
             self.updates += 1
             d = self.decay(self.updates)
 
-            msd = model.module.state_dict() if hasattr(model, 'module') else model.state_dict()
-            esd = self.ema.module.state_dict() if hasattr(self.ema, 'module') else self.ema.state_dict()
-            for k, v in esd.items():
+            msd = model.module.state_dict() if is_parallel(model) else model.state_dict()  # model state_dict
+            for k, v in self.ema.state_dict().items():
                 if v.dtype.is_floating_point:
                     v *= d
                     v += (1. - d) * msd[k].detach()
 
     def update_attr(self, model):
-        # Assign attributes (which may change during training)
+        # Update EMA attributes
         for k, v in model.__dict__.items():
-            # TODO: This is uglyy. Custom attributes should have some specific naming strategy.
-            if not (k.startswith('_') or k in ["process_group", "reducer"] or
-                isinstance(v, (torch.distributed.ProcessGroupNCCL, torch.distributed.Reducer))):
-                try:
-                    pickle.dumps(v)
-                except Exception:
-                    continue
-                else:
-                    setattr(self.ema, k, v)
+            if not k.startswith('_') and k not in ["process_group", "reducer"]:
+                setattr(self.ema, k, v)
